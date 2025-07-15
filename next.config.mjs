6736ca93ff2ea6b700814591ddc583709f35075c
richardburd <rick.a.burd@gmail.com>
2025-07-14 17:25:46 -0700

    env & config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "ram-website-images-and-documents.s3.us-east-2.amazonaws.com",
+      "images.ramtn.org.s3.us-east-2.amazonaws.com",
+    ],
+  },
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+};
 
 export default nextConfig;
