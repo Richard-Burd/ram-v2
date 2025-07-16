dc1cba0b94d534ef5a97ae344894500c29e4a410
richardburd <rick.a.burd@gmail.com>
2025-07-16 12:33:58 -0700

    navbar buttons have proper hover animations

--- src/app/components/NavbarButton.js
+++ src/app/components/NavbarButton.js
@@ -14,7 +14,11 @@ export default function NavbarButton({ title, bgColor }) {
         className={`${bgColor} rounded-full shadow-white`}
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
-        whileHover={{ scale: 1.1, duration: 0.1 }}
+        whileHover={{
+          scale: 1.07,
+          duration: 0.1,
+          boxShadow: "0 0 90px 5px rgba(255, 255, 255, 0.4)",
+        }}
         transition={{ duration: 0.2 }}
       >
         <div className="shadow-lower-right-offset rounded-full">
