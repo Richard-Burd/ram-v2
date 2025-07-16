22d296b6b2d1ae3a92ab2b3b371bb41c3832aedb
richardburd <rick.a.burd@gmail.com>
2025-07-15 20:15:29 -0700

    motion w/framer-motion gone

--- src/app/components/NavbarButton.js
+++ src/app/components/NavbarButton.js
@@ -1,13 +1,28 @@
+"use client"; // Ensure this is present for App Router
+import { motion } from "motion/react";
+
+import { usePathname } from "next/navigation";
+
 export default function NavbarButton({ title, bgColor }) {
+  // state machine to track location in heirarchy
+  const pathname = usePathname();
+  const isActive = pathname.includes(title.toLowerCase().replace(/ /g, "-"));
+
   return (
     <div>
-      <div className={`${bgColor} rounded-full shadow-white`}>
+      <motion.div
+        className={`${bgColor} rounded-full shadow-white`}
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        whileHover={{ scale: 1.1, duration: 0.1 }}
+        transition={{ duration: 0.2 }}
+      >
         <div className="shadow-lower-right-offset rounded-full">
           <div className="pt-serif-700 text-opacity-80 pr-3 pl-2 text-center text-2xl text-black uppercase italic">
             {title}
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 }
